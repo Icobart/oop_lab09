573bdbb9c4d040174237ae2241563e4ba2c3ebc4
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2023-11-25 22:54:29 +0100

    implemented the methods of Controller in class SimpleController

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,43 @@
 package it.unibo.mvc;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private String next;
+    private final List<String> stringHistory = new LinkedList<>();
+
+    @Override
+    public void setNextString(String nextString) {
+        if(Objects.isNull(nextString)) {
+            throw new IllegalArgumentException("null values are not acceptable");
+        }
+        this.next = nextString;
+    }
+
+    @Override
+    public String getNextString() {
+        return this.next;    
+    }
+
+    @Override
+    public List<String> getHistoryStrings() {
+        return new LinkedList<>(this.stringHistory);    
+    }
+
+    @Override
+    public void printCurrentString() {
+        if(Objects.isNull(this.next)) {
+            throw new IllegalStateException("current string is unset");
+        }
+        System.out.println(this.next);
+        this.stringHistory.add(this.next);
+    }
 
 }
