0465ca2b47eff266df5d9ac7db8c193a08bc2fb4
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2023-11-25 23:04:58 +0100

    in SimpleGUI, implemented the method display that will start the graphical application after being called in the main

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,5 +1,8 @@
 package it.unibo.mvc;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 /**
@@ -8,6 +11,26 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
+    private static final int PROPORTION = 2;
+
     private final JFrame frame = new JFrame();
+
+    private SimpleGUI(final Controller controller) {
+        
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... a) {
+        final SimpleGUI gui = new SimpleGUI(new SimpleController());
+        gui.display();
+    }
 
 }
