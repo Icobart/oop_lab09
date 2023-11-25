80bfa2ddcf51b26202a4dbd2aa5e31fddeec2941
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2023-11-25 22:48:34 +0100

    created the methods in the Controller interface

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,31 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * 
  */
 public interface Controller {
 
+    /**
+     * @param nextString next string to print on standard output
+     */
+    public void setNextString(String nextString);
+
+    /**
+     * 
+     * @return the next string to print
+     */
+    public String getNextString();
+
+    /**
+     * 
+     * @return the history of the printed strings
+     */
+    public List<String> getHistoryStrings();
+
+    /**
+     * 
+     */
+    public void printCurrentString();
 }
