42026087a982ab1c9343ca5a6b18fdfa70846674
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2023-11-25 21:20:55 +0100

    implemented the SimpleGui and added javadoc to both Controller and SimpleGUI

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -15,6 +15,11 @@ public class Controller {
 
     private File fileCurr = new File(USERHOME + File.separator + OUTPUTFILE);
 
+    /**
+     * Sets a file as current file
+     * 
+     * @param current file to set as current file
+     */
     public void setCurrentFile(final File current) {
         if(current.getParentFile().exists()) {
             this.fileCurr = current;
@@ -24,18 +29,38 @@ public class Controller {
         }
     }
 
+    /**
+     * Sets a file as current file
+     * 
+     * @param current file to set as current file
+     */
     public void setCurrentFile(final String current) {
         setCurrentFile(new File(current));
     }
 
+    /**
+     * Return the current file
+     * 
+     * @return current file
+     */
     public File getCurrentFile() {
         return this.fileCurr;
     }
 
+    /**
+     * Return the current file path
+     * 
+     * @return current file path
+     */
     public String getCurrentStringFile() {
         return this.fileCurr.getPath();
     }
 
+    /**
+     * Saves the text written
+     * @param inputString text that has to be saved
+     * @throws IOException if save fails
+     */
     public void fetchAndWrite (final String inputString) throws IOException{
         try (PrintStream ps = new PrintStream(fileCurr, StandardCharsets.UTF_8)) {
             ps.println(inputString);
--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,17 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +19,51 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 2;
+
+    private final JFrame frame = new JFrame("SimpleGui");
+
+    public SimpleGUI(final Controller controller) {
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JTextArea text = new JTextArea();
+        canvas.add(text, BorderLayout.CENTER);
+        final JButton save = new JButton("Save");
+        canvas.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        save.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    controller.fetchAndWrite(text.getText());
+                } catch (IOException e1) {
+                    JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+            
+        });
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    /**
+     * Launches the application
+     * 
+     * @param a unused
+     * 
+     */
+    public static void main(final String... a) {
+        final SimpleGUI gui = new SimpleGUI(new Controller());
+        gui.display();
+    }
 
 }
