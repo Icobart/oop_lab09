a6b9af1e54d3c73188eb4eda1df067e40eba5804
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2023-11-26 15:26:09 +0100

    implemented LaunchApp with reflections

--- 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,10 +1,11 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.InvocationTargetException;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberStdoutView;
-import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
  * Application entry-point.
@@ -24,11 +25,31 @@ public final class LaunchApp {
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws
+        ClassNotFoundException,
+        NoSuchMethodException,
+        InvocationTargetException,
+        InstantiationException,
+        IllegalAccessException,
+        IllegalArgumentException {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberStdoutView());
+        final var drawst = Class.forName("it.unibo.mvc.view.DrawNumberStdoutView");
+        final var drawswing = Class.forName("it.unibo.mvc.view.DrawNumberSwingView");
+        for(int i=0; i<3; i++) {
+            final var stdout = drawst.getConstructor().newInstance();
+            final var swing = drawswing.getConstructor().newInstance();
+            if(DrawNumberView.class.isAssignableFrom(stdout.getClass())) {
+                app.addView((DrawNumberView)stdout);
+            } else {
+                throw new IllegalStateException(stdout.getClass() + " not valid");
+            }
+            if(DrawNumberView.class.isAssignableFrom(swing.getClass())) {
+                app.addView((DrawNumberView)swing);
+            } else {
+                throw new IllegalStateException(swing.getClass() + " not valid");
+            }
+        }
+        
     }
 }
